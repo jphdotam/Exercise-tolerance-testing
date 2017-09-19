3726c494d8d8ad8b3d5811c1f3a280425d7c17da
James <github@jph.am>
2017-09-19 16:13:15 +0100

    Initial committ
    Signed-off-by: James <github@jph.am>

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from ECGdata import ECGData
 from ProcessedECG import ProcessedECG
-import pickle
 
 patient = "ORBITA21386"
 
 ecg_data = ECGData(patient)
 processed_ecg = ProcessedECG(ecg_data)
-qrsd,st = processed_ecg.calculate()
+qrsd,st = processed_ecg.plot()
 
 print("QRSd: {}".format(qrsd))
 print("ST: {}".format(st))
